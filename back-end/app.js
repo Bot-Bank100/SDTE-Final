de37c3c028ca9c72103ce7a415b4afffff52acbf
Bot-Bank100 <56309214+Bot-Bank100@users.noreply.github.com>
2022-05-09 17:37:32 +0700

    Add front-end and back-end

---
+++ back-end/app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+const { MongoClient } = require('mongodb');
+const database = 'mongodb://localhost:27017';
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello!' })
+  })
+
+  
+app.get('/users/:username', async(req, res) => {
+  const username = req.params.username
+  const client = new MongoClient(database);
+  await client.connect();
+  const user = await client.db('Test').collection('Test').findOne({"username": username})
+  await client.close()
+  res.status(200).send({
+    "status": "ok",
+    "user": user
+  })
+})
+
+app.post('/users/create', async(req, res) => {
+  const user = req.body;
+  console.log(user)
+  const client = new MongoClient(database);
+  await client.connect();
+  await client.db('Test').collection('Test').insertOne({
+    id: parseInt(user.id),
+    fname: user.fname,
+    lname: user.lname,
+    username: user.username,
+    email: user.email,
+  });
+  await client.close();
+  res.status(200).send({
+    "status": "ok",
+    "message": "User with ID = '+user.id+' is created",
+    "user": user
+  });
+})
+
+app.listen(3000, () => {
+    console.log('Application is running on port 3000')
+})
